665df6ac82e36507242e669a6497f4ce2db25b97
John Ochoa <john.ochoa1024@gmail.com>
2023-12-22 13:01:55 -0500

    consulta-automotores

---
+++ src/main/java/com/consulta/automotores/ConsultaAutomotoresApplication.java
@@ -0,0 +1,13 @@
+package com.consulta.automotores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaAutomotoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultaAutomotoresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/consulta/automotores/ConsultaAutomotoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulta.automotores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaAutomotoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
